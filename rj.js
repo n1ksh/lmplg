84d43add1e006e1d11b8172ccb40d48e0619bcd7
n1ksh <99229505+n1ksh@users.noreply.github.com>
2022-02-08 05:34:25 +0300

    Update rj.js

--- rj.js
+++ rj.js
@@ -1773,13 +1773,5 @@
       }
     });
 
-    function () {
-    'use strict';	
-    Lampa.Listener.follow('full', function (e) {
-        if (e.type == 'complite') {
-            e.object.activity.render().find('.view--trailer').remove();
-        }
-    });
-};
-    
+        
 })();
